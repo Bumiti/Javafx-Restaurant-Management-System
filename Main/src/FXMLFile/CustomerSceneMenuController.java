f4be2a847a51fe48cdbad3f3ec30932fe0416e41
Buivanbi <dominicus.minh@gmail.com>
2022-12-17 17:43:17 +0700

    Minh

---
+++ Main/src/FXMLFile/CustomerSceneMenuController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package FXMLFile;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author admin
+ */
+public class CustomerSceneMenuController implements Initializable {
+
+    @FXML
+    private Tab tabTable;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
+}
